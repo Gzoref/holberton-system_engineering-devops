eb27283c7c76a4154ca37ab8a1fde6332f9a325c
Gzoref <gzoref@gmail.com>
2020-03-24 00:54:58 -0400

    Fix puppet code

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,11 @@
 #client SSH configuration file to connect to server without typing a password.
-file_line { 'Declare identify file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentifyFile ~/.ssh/holberton',
-}
-
 file_line { 'Turn off passwd auth':
   ensure => 'present',
-  path   => 'etc/ssh_config',
-  line   => 'PasswordAuthentification no',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/holberton',
+  }
