0ea0a4e93cd8e447758055e669b250878fad50e0
Gzoref <gzoref@gmail.com>
2020-03-26 16:07:21 -0400

    Puppet config

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,7 @@
+#Install Nginx web server (w/ Puppet)
+
+
+exec { 'Install:
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i '/^\sserver_name.*/a \        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default && sudo service nginx start'
+  provider => shell,
+}
