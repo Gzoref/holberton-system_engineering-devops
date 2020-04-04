d4bcd46b567550ff13e6ca3e08ba4eac74422894
Gzoref <gzoref@gmail.com>
2020-04-03 21:12:39 -0400

    Config with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
 #Add a custom HTTP header with Puppet
 
 exec { 'Install':
-       command  =>  'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i "/^\sserver_name.*/a \                     rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && sudo sed -i \'/^\slocation.*/i \        add_header X-Served-By $hostname;'                   etc/nginx/sites-available/default && sudo service nginx start',
-       provider => shell,
+  command  => 'INDEX="Holberton School for the win!" && ERROR="Ceci n\'est pas une page - 404" && sudo apt-get -y update && sudo apt-get -y install nginx && echo "$INDEX" | sudo tee /var/www/html/index.nginx-debian.html > /dev/null && echo "$ERROR" | sudo tee /var/www/html/custom_404.html > /dev/null && sudo sed -i \'/^\sserver_name.*/a \        rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\' /etc/nginx/sites-available/default && sudo sed -i \'/^\slocation.*/i \        error_page 404 /custom_404.html;\' /etc/nginx/sites-available/default && sudo sed -i \'/^\slocation.*/i \        add_header X-Served-By $hostname;\' /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell,
 }
+
